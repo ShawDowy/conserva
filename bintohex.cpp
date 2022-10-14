820572a50bb1ab20f70d59c8ac33ef36020b0a91
ShawDowy <v4rfolomeev4322@yandex.ru>
2022-10-14 23:13:27 +0300

    z

--- bintohex.cpp
+++ bintohex.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
 #include <cmath>
 
+
 using namespace std;
+
 
 int strlen_(char* bin)
 {
-	int counter = 0;
 	int i = 0;
 	while (bin[i])
 	{
@@ -14,7 +15,8 @@ int strlen_(char* bin)
 	return i;
 }
 
-int find_symbol_index(char* bin)
+
+int find_dot_index(char* bin)
 {
 	int index = 0;
 	for (int i = 0; i < strlen_(bin); ++i)
@@ -32,96 +34,100 @@ int find_symbol_index(char* bin)
 
 int bin_to_hex(char* bin, int& len)
 {
-	if (find_symbol_index(bin) > 0)
+	if (find_dot_index(bin) > 0)
 	{
-		int len2 = find_symbol_index(bin) + (4 - (find_symbol_index(bin) % 4));
-		char* bin_2 = new char[len2];
+		int arrlen_integer = find_dot_index(bin);
+		char* bin_integer = new char[arrlen_integer+1];
 
-		for (int i = 0; i < 4 - (find_symbol_index(bin) % 4); ++i)
+
+		for (int i = 0; i < arrlen_integer; ++i)
 		{
-			bin_2[i] = '0';
+			bin_integer[i] = bin[i];
 		}
 
-		for (int i = 0; i < find_symbol_index(bin); ++i)
-		{
-			bin_2[i + (4 - (find_symbol_index(bin) % 4))] = bin[i];
-		}
 
 		int z = 0;
-		int dec_2 = 0;
+		int dec_integer = 0;
 
-		while (z < len2)
+
+		while (z < arrlen_integer)
 		{
-			dec_2 += ((int)bin_2[z] - 48) * pow(2, len2 - (1 + z));
+			dec_integer += ((int)bin_integer[z] - 48) * pow(2, arrlen_integer - (1 + z));
 			++z;
 		}
-		return dec_2;
+		delete[] bin_integer;
+
+		return dec_integer;
 	}
 	else
 	{
-		char* bin_2 = new char[len];
-
-		for (int i = 0; i < len; ++i)
-		{
-			bin_2[i] = bin[i];
-		}
-
 		int z = 0;
-		int dec_2 = 0;
+		int dec_integer = 0;
+
+
 		while (z < len)
 		{
-			dec_2 += ((int)bin_2[z] - 48) * pow(2, len - (1 + z));
+			dec_integer += ((int)bin[z] - 48) * pow(2, len - (1 + z));
 			++z;
 		}
-		return dec_2;
+		return dec_integer;
 	}
 }
 
 
 int main()
 {
-	char bin2[150];
-	cin >> bin2;
+	char bin[150];
+
+	cin >> bin;
 
 
-	int arrlen = strlen_(bin2);
+	int arrlen = strlen_(bin);
 
-	cout << hex << bin_to_hex(bin2, arrlen);
 
-	if (find_symbol_index(bin2) > 0)
+	cout << hex << bin_to_hex(bin, arrlen);
+
+
+	if (find_dot_index(bin) > 0)
 	{
 		cout << '.';
 
-		int len2 = (arrlen - (find_symbol_index(bin2) + 1)) + (4 - ((arrlen - (find_symbol_index(bin2) + 1)) % 4));
-		char* bin_3 = new char[len2];
 
-		for (int i = 0; i < (arrlen - (find_symbol_index(bin2) + 1)); ++i)
+		int arrlen_float = (arrlen - (find_dot_index(bin) + 1)) + (4 - ((arrlen - (find_dot_index(bin) + 1)) % 4) );
+		char* bin_float = new char[arrlen_float+1];
+
+
+		for (int i = 0; i < (arrlen - (find_dot_index(bin) + 1)); ++i)
 		{
-			bin_3[i] = bin2[i + (find_symbol_index(bin2)) + 1];
+			bin_float[i] = bin[i + (find_dot_index(bin)) + 1];
 		}
 
-		for (int i = 0; i < (4 - ((arrlen - (find_symbol_index(bin2) + 1)) % 4)); ++i)
+		for (int i = 0; i < (4 - ((arrlen - (find_dot_index(bin) + 1)) % 4)); ++i)
 		{
-			bin_3[i + (arrlen - (find_symbol_index(bin2) + 1))] = '0';
+			bin_float[i + (arrlen - (find_dot_index(bin) + 1))] = '0';
 		}
+
 
 		int z = 0;
 		int dec_3 = 0;
 		int f_ = 4;
 
-		while (z < len2)
+
+		while (z < arrlen_float)
 		{
 			int f = z + 1;
-			dec_3 += ((int)bin_3[z] - 48) * pow(2, f_ - f);
+			dec_3 += ((int)bin_float[z] - 48) * pow(2, f_ - f);
 			if (f % 4 == 0)
 			{
 				cout << hex << dec_3;
-				dec_3 = 0;
 				f_ += 4;
+				dec_3 = 0;
 			}
 			++z;
 		}
+		delete[] bin_float;
+
+		return 0;
 	}
 	return 0;
 }
-
